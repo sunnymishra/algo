ec13c54cff2e0caa1ce23e88fd564b6a98ca0ac7
sunnymishra <sunny.mishra0389@gmail.com>
2020-09-11 21:59:24 +0530

    Added DP problems: Longest common Subsequence strings, Team score sequence

--- src/dynamicprogramming/StairClimbing.java
+++ src/dynamicprogramming/StairClimbing.java
@@ -2,15 +2,13 @@ package dynamicprogramming;
 
 import org.junit.Test;
 import org.junit.Assert;
-import java.util.Map;
-import java.util.HashMap;
 import java.util.Arrays;
 
 public class StairClimbing{
 	@Test
 	public void stairClimbing1(){
-		Assert.assertEquals(7, stairClimbing1(6, 3));
-		Assert.assertEquals(5, stairClimbing1(5, 3));
+		Assert.assertEquals(24, stairClimbing1(6, 3));
+		
 		Assert.assertEquals(5, stairClimbing1(4, 2));
 	}
 	
@@ -22,30 +20,41 @@ public class StairClimbing{
 		
 		Solution logic:
 		This is again a variation of Zero One knapsack problem, very similar to Coin change problem.
-		Below code is a Top-down Recursion approach. Next I will also add the Bottom-Up iterative approach with 
-		Memoization(to store intermediate steps) to add optimization in the Space and time complexity.
+		Below code is a Top-down Recursion approach. I have added the Memoization(to store intermediate steps) in a Cache array to add optimization in the Space and time complexity.
 		
-		Time complexity = O(2^n), which is an exponential solution.
-		Space complexity = O(n)
+		Time complexity = O(n x k), where n is the totalSteps and k is the steps allowed
+		Space complexity = O(n),  where n is the totalSteps. How? If you draw the recursion tree with paper and pen, the max depth of the tree is at max the totalSteps in the worst case when reduced by a factor of '1' at each level i.e. if at each level '1' step is taken. Also, the Caching in Array is also equal to size n. Thus Space complexity=O(n)
+		
+		NOTE: If in below code I remove the Memoization step i.e. the storing of intermediate results in the Cache array, then the Time complexity will significantly increase from current O(k x n) to O(k ^ n)
 	*/
 	public int stairClimbing1(int totalSteps, int steps){
+		System.out.println("\n");
 		if(steps<=0 || totalSteps<=0){
 			return 0;
 		}
-		int totCombination = _stairClimbing1(totalSteps, steps);
-
+		int[] cache=new int[totalSteps];
+		
+		int totCombination = _stairClimbing1(steps, totalSteps, cache);
+		
+		System.out.println(Arrays.toString(cache)+"\n\nResult: "+ totCombination);
+		
 		return totCombination;
 	}
 	
-	private int _stairClimbing1(int totalSteps, int stepsIdx){
+	private int _stairClimbing1(int allowedSteps, int totalSteps, int[] cache){
 		if(totalSteps==0) return 1;
 		if(totalSteps<0) return 0;
-		if(stepsIdx<1) return 0;
-		
-		int sumInclude=_stairClimbing1((totalSteps-stepsIdx), stepsIdx);
-		int sumExclude=_stairClimbing1((totalSteps), stepsIdx-1);
-		
-		return sumInclude+sumExclude;
+
+		if(cache[totalSteps-1]!=0){
+			return cache[totalSteps-1];
+		}
+
+		int ways=0;
+		for(int i=1; i<=allowedSteps; i++){
+			ways+=_stairClimbing1(allowedSteps, (totalSteps-i), cache);
+		}
+		cache[totalSteps-1]=ways;
+		return ways;
 	}
 	
 }
---
+++ src/dynamicprogramming/TeamScoreSequence.java
@@ -0,0 +1,79 @@
+package dynamicprogramming;
+
+import org.junit.Test;
+import org.junit.Assert;
+import java.util.Arrays;
+
+public class TeamScoreSequence{
+	@Test
+	public void teamScoreSequence(){
+		Assert.assertEquals(5, teamScoreSequence1(new int[]{1,2}, 2, 1));
+		
+	}
+	
+	/**
+		Reference: https://stackoverflow.com/a/13714149/1316967
+		If there is a basketball match and the 2 team's final scores are (TeamA=3, TeamB=2), then what are the possible ways in which teams which started with 0, 0 could have reached that final score? Given that in Basketball any team is allowed to score among following 3 points at any time : 1, 2
+		
+	**/
+	public int teamScoreSequence1(int[] points, int teamAScore, int teamBScore){
+		if(points==null || points.length==0){
+			return 0;
+		}
+		int combinations= _teamScoreSequence1(points, teamAScore, teamBScore);
+		System.out.println("No. of ways: "+ combinations);
+		return combinations;
+	}
+	
+	private int _teamScoreSequence1(int[] points, int teamAScore, int teamBScore){
+		if(teamAScore<0 || teamBScore<0){
+			return 0;
+		}
+		if(teamAScore==0 && teamBScore==0){
+			return 1;
+		}
+		int sum=0;
+		for(int i=0; i<points.length; i++){
+			int score1 = _teamScoreSequence1(points, teamAScore-points[i], teamBScore); // Team A score combinations
+			int score2 = _teamScoreSequence1(points, teamAScore, teamBScore-points[i]); // Team B score combinations
+			sum=sum + score1+score2;
+		}
+		return sum;
+	}
+	
+	
+	public int teamScoreSequence2(int[] points, int teamAScore, int teamBScore){
+		if(points==null || points.length==0){
+			return 0;
+		}
+		System.out.println("teamAScore: "+ teamAScore + " teamBScore: "+teamBScore);
+		int[][]cache=new int[teamAScore][teamBScore];
+		
+		int combinations= _teamScoreSequence2(points, teamAScore, teamBScore, cache);
+		for(int i=0;i<teamAScore;i++){
+			System.out.println(Arrays.toString(cache[i]));
+		}
+		return combinations;
+	}
+	
+	private int _teamScoreSequence2(int[] points, int teamAScore, int teamBScore, int[][]cache){
+		if(teamAScore<1 || teamBScore<1){
+			return 0;
+		}
+		if(teamAScore==0 && teamBScore==0){
+			return 1;
+		}
+		if(cache[teamAScore-1][teamBScore-1]!=0){
+			return cache[teamAScore-1][teamBScore-1];
+		}
+		int sum=0;
+		for(int i=0; i<points.length; i++){
+			int score1 = _teamScoreSequence2(points, teamAScore-points[i], teamBScore, cache); // Team A score combinations
+			int score2 = _teamScoreSequence2(points, teamAScore, teamBScore-points[i], cache); // Team B score combinations
+			sum=sum + score1+score2;
+		}
+		cache[teamAScore-1][teamBScore-1]=sum;
+		return sum;
+	}
+	
+}
